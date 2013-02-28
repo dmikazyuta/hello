06bbe174a3c43783a38a0a3e85747102c74fa29a
dmitriy.kazyuta <dmitriy.kazyuta@gmail.com>
2013-02-28 14:09:59 +0300

    beautiful commit

--- Hallo/src/uralchem/kckk/uit/FirstHallo.java
+++ Hallo/src/uralchem/kckk/uit/FirstHallo.java
@@ -3,6 +3,6 @@ package uralchem.kckk.uit;
 public class FirstHallo {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World!");
+		System.out.println("Hello Beautiful World!");
 	}
 }

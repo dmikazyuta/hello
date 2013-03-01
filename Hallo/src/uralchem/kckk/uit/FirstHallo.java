958f7354d1b27b61d68b4a71c2aaf96c435bc969
dmitriy.kazyuta <dmitriy.kazyuta@gmail.com>
2013-03-01 12:56:27 +0300

    commit after recreate angry extended version

---
+++ Hallo/src/uralchem/kckk/uit/FirstHallo.java
@@ -0,0 +1,10 @@
+package uralchem.kckk.uit;
+
+public class FirstHallo {
+	public static void main(String args[])
+	{
+		String text = "Hello world!";
+		System.out.println(text);
+		System.out.println("Hello Angry World!");
+	}
+}
